597bd0f20ba95d461a7819cad59479a221eb587b
jngcii <concotree@gmail.com>
2019-08-31 15:13:44 +0900

    Create : sign up function by bcrypt, json-web-token

--- src/api/User/signup/signup.js
+++ src/api/User/signup/signup.js
@@ -1,5 +1,21 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { APP_SECRET } = require("../../../utils");
+
 export default {
-	Query: {
-		something: () => "Hello"
+	Mutation: {
+		signup: async (_, { username, name, password: pw }, context) => {
+			const password = await bcrypt.hash(pw, 10);
+
+			const user = await context.prisma.createUser({
+				username,
+				name,
+				password
+			});
+
+			const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+			return { user, token };
+		}
 	}
 };
--- src/server.js
+++ src/server.js
@@ -2,10 +2,20 @@ require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
+import { prisma } from "../generated/prisma-client";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({ schema });
+const server = new GraphQLServer({
+	schema,
+	context: request => {
+		return {
+			...request,
+			prisma
+		};
+	}
+});
+
 server.express.use(logger("dev"));
 
 server.start({ port: PORT }, () =>
