8a89a600d9faa6552ea1ad4713a3346cfe34f557
jngcii <concotree@gmail.com>
2019-08-31 23:44:24 +0900

    Create : add single itemAct to plan

--- src/api/ItemAct/addItemAct/addItemAct.js
+++ src/api/ItemAct/addItemAct/addItemAct.js
@@ -15,15 +15,13 @@ export default {
 				if (newItemActs.length == i) {
 					const itemAct = await context.prisma.createItemAct({
 						keyword: itemActs[i].keyword,
-						color: itemActs[i].color,
+						color: itemActs[i].color || "#333",
 						isChecked: false,
 						parentItem: itemActs[i].parentItem || null,
 						childItems: itemActs[i].childItems || null,
 						memo: "",
 						order: itemActs[i].order
 					});
-
-					console.log(itemAct);
 
 					newItemActs.push(itemAct);
 				}
@@ -37,6 +35,27 @@ export default {
 					else return newItemActs;
 				});
 			}
+		},
+
+		addItemAct: async (_, { itemAct, planId }, context) => {
+			try {
+				getUserId(context);
+			} catch {
+				return false;
+			}
+
+			const res = await context.prisma.createItemAct({
+				plan: { connect: { id: planId } },
+				keyword: itemAct.keyword,
+				color: itemAct.color || "#333",
+				isChecked: false,
+				parentItem: itemAct.parentItem || null,
+				childItems: itemAct.childItems || null,
+				memo: "",
+				order: itemAct.order
+			});
+
+			if (res) return true;
 		}
 	}
 };
